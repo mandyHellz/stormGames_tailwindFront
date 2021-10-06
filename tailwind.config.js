be210fee4d59ec350d7e01f21a59ffc85ab5ae3e
Amanda <carvalho_amanda@hotmail.com>
2021-10-05 23:46:37 -0300

    first commit: added header

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      maxWidth: {
+        container: '1024px',
+        page: '1920px',
+      },
+      fontFamily: {
+        'Roboto': ['Roboto', 'sans-serif'],
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
