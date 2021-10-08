02e32ad9c6421cf06c6d1089d3abb8c62bfa2e46
Amanda Nunes <amandanunes@Amandas-Mac-mini.local>
2021-10-08 15:14:02 -0300

    Update style.css file for global use

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,10 @@ module.exports = {
           default: '#2A2D40',
           darken: '#1B1D2A',
         }
-      }
+      },
+      backgroundImage:{
+        darkbackground: "url('https://img.rawpixel.com/s3fs-private/rawpixel_images/website_content/v882-sasi-13.jpg?w=800&dpr=1&fit=default&crop=default&q=65&vib=3&con=3&usm=15&bg=F4F4F3&ixlib=js-2.2.1&s=012f0abdf565090eb02bdeb9f97d0f88')",
+      },
     }
   },
   variants: {
